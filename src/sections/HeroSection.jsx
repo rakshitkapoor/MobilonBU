0e32ba3c8ad990939f1d09dee790eb386e8b248b
slaygun <nabhraghav@gmail.com>
2024-03-01 11:02:04 +0530

    hero tweaks

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -51,11 +51,11 @@ export function HeroSection() {
       <div className="text w-fit">
         <h1 className="font-bold text-6xl md:text-9xl
           text-purple-300">MOBILON BU</h1>
-        <p className="md:text-xl px-1 w-fit">A Mobile Development Club at Bennett University</p>
+        <p className="md:text-xl px-1 text-gray-400 w-fit">A Mobile Development Club at Bennett University</p>
         <div className="py-4 flex space-x-4 text-purple-400 md:w-fit">
-          <Mail className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-white hover:border-white  rounded-xl cursor-pointer"/>
-          <Instagram className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-white hover:border-white rounded-xl cursor-pointer"/>
-          <Linkedin className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-white hover:border-white rounded-xl cursor-pointer" />
+          <Mail className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-purple-300 hover:border-purple-300  rounded-xl cursor-pointer"/>
+          <Instagram className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-purple-300 hover:border-purple-300 rounded-xl cursor-pointer"/>
+          <Linkedin className="w-12 h-12 p-1.5 border-2 border-purple-400 hover:text-purple-300 hover:border-purple-300 rounded-xl cursor-pointer" />
         </div>
       </div>
       <Slider/>
